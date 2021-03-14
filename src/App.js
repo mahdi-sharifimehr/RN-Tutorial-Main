0add2b8150a42313f5a2c0cefb57fe66f3cdb65c
Mahdi <mathfractal@yahoo.com>
2021-03-14 23:33:59 +0330

    commit

--- src/App.js
+++ src/App.js
@@ -1,65 +1,71 @@
 
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import ScreenA from './ScreenA';
 import ScreenB from './ScreenB';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
-// const Tab = createBottomTabNavigator();
-// const Tab = createMaterialBottomTabNavigator();
-const Tab = createMaterialTopTabNavigator();
+const Drawer = createDrawerNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, size, color }) => {
-            let iconName;
-            if (route.name === 'Screen_A') {
-              iconName = 'autoprefixer';
-              size = focused ? 25 : 20;
-              // color = focused ? '#f0f' : '#555';
-            } else if (route.name === 'Screen_B') {
-              iconName = 'btc';
-              size = focused ? 25 : 20;
-              // color = focused ? '#f0f' : '#555';
-            }
-            return (
-              <FontAwesome5
-                name={iconName}
-                size={size}
-                color={color}
-              />
-            )
-          }
-        })}
-        tabBarOptions={{
-          activeTintColor: '#f0f',
-          inactiveTintColor: '#555',
-          activeBackgroundColor: '#fff',
-          inactiveBackgroundColor: '#999',
-          showLabel: true,
-          labelStyle: { fontSize: 14 },
-          showIcon: true,
+      <Drawer.Navigator
+        initialRouteName="Screen_A"
+        drawerPosition='left'
+        drawerType="front"
+        edgeWidth={100}
+        hideStatusBar={false}
+        overlayColor='#00000090'
+        drawerStyle={{
+          backgroundColor: '#e6e6e6',
+          width: 250
         }}
-        activeColor='#f0edf6'
-        inactiveColor='#3e2465'
-        barStyle={{ backgroundColor: '#694fad' }}
+        screenOptions={{
+          headerShown: true,
+          swipeEnabled: true,
+          gestureEnabled: true,
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#0080ff'
+          },
+          headerTintColor: '#ffffff',
+          headerTitleStyle: {
+            fontSize: 25,
+            fontWeight: 'bold'
+          }
+        }}
       >
-        <Tab.Screen
+        <Drawer.Screen
           name="Screen_A"
           component={ScreenA}
-          options={{ tabBarBadge: 3 }}
+          options={{
+            title: 'Screen_A Title',
+            drawerIcon: ({ focused }) => (
+              <FontAwesome5
+                name="autoprefixer"
+                size={focused ? 25 : 20}
+                color={focused ? '#0080ff' : '#999999'}
+              />
+            )
+          }}
         />
-        <Tab.Screen
+        <Drawer.Screen
           name="Screen_B"
           component={ScreenB}
+          options={{
+            title: 'Screen_B Title',
+            drawerIcon: ({ focused }) => (
+              <FontAwesome5
+                name="btc"
+                size={focused ? 25 : 20}
+                color={focused ? '#0080ff' : '#999999'}
+              />
+            )
+          }}
         />
-      </Tab.Navigator>
+      </Drawer.Navigator>
     </NavigationContainer>
   )
 }
