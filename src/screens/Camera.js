8b5a5eb30aba6dae371d2d77e535c6e8e3f54c17
Mahdi <mathfractal@yahoo.com>
2021-07-31 23:50:04 +0430

    commit

--- src/screens/Camera.js
+++ src/screens/Camera.js
@@ -2,31 +2,43 @@ import React from 'react';
 import {
     View,
     StyleSheet,
+    Alert,
 } from 'react-native';
 import { RNCamera } from 'react-native-camera';
 import { useCamera } from 'react-native-camera-hooks';
 import CustomButton from '../utils/CustomButton';
-import RNFS from 'react-native-fs';
+import { useDispatch, useSelector } from 'react-redux';
+import { setTasks } from '../redux/actions';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export default function Camera() {
+export default function Camera({ navigation, route }) {
 
     const [{ cameraRef }, { takePicture }] = useCamera(null);
+    const { tasks } = useSelector(state => state.taskReducer);
+    const dispatch = useDispatch();
 
     const captureHandle = async () => {
         try {
             const data = await takePicture();
-            console.log(data.uri);
             const filePath = data.uri;
-            const newFilePath = RNFS.ExternalDirectoryPath + '/MyTest.jpg';
-            RNFS.moveFile(filePath, newFilePath)
-                .then(() => {
-                    console.log('IMAGE MOVED', filePath, '-- to --', newFilePath);
-                })
-                .catch(error => {
-                    console.log(error);
-                })
+            updateTask(route.params.id, filePath);
         } catch (error) {
             console.log(error);
+        }
+    }
+
+    const updateTask = (id, path) => {
+        const index = tasks.findIndex(task => task.ID === id);
+        if (index > -1) {
+            let newTasks = [...tasks];
+            newTasks[index].Image = path;
+            AsyncStorage.setItem('Tasks', JSON.stringify(newTasks))
+                .then(() => {
+                    dispatch(setTasks(newTasks));
+                    Alert.alert('Success!', 'Task image is saved.');
+                    navigation.goBack();
+                })
+                .catch(err => console.log(err))
         }
     }
 
--- src/screens/Task.js
+++ src/screens/Task.js
@@ -1,10 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import { Alert, StyleSheet, Text, View } from 'react-native'
+import { Alert, Modal, StyleSheet, Text, TouchableOpacity, View, Image, ScrollView } from 'react-native'
 import { TextInput } from 'react-native-gesture-handler'
 import CustomButton from '../utils/CustomButton';
 import { useDispatch, useSelector } from 'react-redux';
 import { setTasks } from '../redux/actions';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import CheckBox from '@react-native-community/checkbox';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import PushNotification from 'react-native-push-notification';
+import RNFS from 'react-native-fs';
 
 export default function Task({ navigation }) {
 
@@ -13,9 +17,16 @@ export default function Task({ navigation }) {
 
     const [title, setTitle] = useState('');
     const [desc, setDesc] = useState('');
+    const [done, setDone] = useState(false);
+    const [showBellModal, setShowBellModal] = useState(false);
+    const [color, setColor] = useState('white');
+    const [bellTime, setBellTime] = useState('1');
+    const [image, setImage] = useState('');
 
     useEffect(() => {
-        getTask();
+        navigation.addListener('focus', () => {
+            getTask();
+        });
     }, [])
 
     const getTask = () => {
@@ -23,6 +34,9 @@ export default function Task({ navigation }) {
         if (Task) {
             setTitle(Task.Title);
             setDesc(Task.Desc);
+            setDone(Task.Done);
+            setColor(Task.Color);
+            setImage(Task.Image);
         }
     }
 
@@ -34,7 +48,10 @@ export default function Task({ navigation }) {
                 var Task = {
                     ID: taskID,
                     Title: title,
-                    Desc: desc
+                    Desc: desc,
+                    Done: done,
+                    Color: color,
+                    Image: image
                 }
                 const index = tasks.findIndex(task => task.ID === taskID);
                 let newTasks = [];
@@ -57,28 +74,201 @@ export default function Task({ navigation }) {
         }
     }
 
+    const setTaskAlarm = () => {
+        PushNotification.localNotificationSchedule({
+            channelId: 'task-channel',
+            title: title,
+            message: desc,
+            date: new Date(Date.now() + parseInt(bellTime) * 60 * 1000),
+            allowWhileIdle: true,
+        });
+    }
+
+    const deleteImage = () => {
+        RNFS.unlink(image)
+            .then(() => {
+                const index = tasks.findIndex(task => task.ID === taskID);
+                if (index > -1) {
+                    let newTasks = [...tasks];
+                    newTasks[index].Image = '';
+                    AsyncStorage.setItem('Tasks', JSON.stringify(newTasks))
+                        .then(() => {
+                            dispatch(setTasks(newTasks));
+                            getTask();
+                            Alert.alert('Success!', 'Task image is removed.');
+                        })
+                        .catch(err => console.log(err))
+                }
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
-        <View style={styles.body}>
-            <TextInput
-                value={title}
-                style={styles.input}
-                placeholder='Title'
-                onChangeText={(value) => setTitle(value)}
-            />
-            <TextInput
-                value={desc}
-                style={styles.input}
-                placeholder='Description'
-                multiline
-                onChangeText={(value) => setDesc(value)}
-            />
-            <CustomButton
-                title='Save Task'
-                color='#1eb900'
-                style={{ width: '100%' }}
-                onPressFunction={setTask}
-            />
-        </View>
+        <ScrollView>
+            <View style={styles.body}>
+                <Modal
+                    visible={showBellModal}
+                    transparent
+                    onRequestClose={() => setShowBellModal(false)}
+                    animationType='slide'
+                    hardwareAccelerated
+                >
+                    <View style={styles.centered_view}>
+                        <View style={styles.bell_modal}>
+                            <View style={styles.bell_body}>
+                                <Text style={styles.text}>Remind me After</Text>
+                                <TextInput
+                                    style={styles.bell_input}
+                                    keyboardType='numeric'
+                                    value={bellTime}
+                                    onChangeText={(value) => setBellTime(value)}
+                                />
+                                <Text style={styles.text}>minute(s)</Text>
+                            </View>
+                            <View style={styles.bell_buttons}>
+                                <TouchableOpacity
+                                    style={styles.bell_cancel_button}
+                                    onPress={() => {
+                                        setShowBellModal(false)
+                                    }}
+                                >
+                                    <Text style={styles.text}>Cancel</Text>
+                                </TouchableOpacity>
+                                <TouchableOpacity
+                                    style={styles.bell_ok_button}
+                                    onPress={() => {
+                                        setShowBellModal(false)
+                                        setTaskAlarm()
+                                    }}
+                                >
+                                    <Text style={styles.text}>OK</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+                <TextInput
+                    value={title}
+                    style={styles.input}
+                    placeholder='Title'
+                    onChangeText={(value) => setTitle(value)}
+                />
+                <TextInput
+                    value={desc}
+                    style={styles.input}
+                    placeholder='Description'
+                    multiline
+                    onChangeText={(value) => setDesc(value)}
+                />
+                <View style={styles.color_bar}>
+                    <TouchableOpacity
+                        style={styles.color_white}
+                        onPress={() => { setColor('white') }}
+                    >
+                        {color === 'white' &&
+                            <FontAwesome5
+                                name={'check'}
+                                size={25}
+                                color={'#000000'}
+                            />
+                        }
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.color_red}
+                        onPress={() => { setColor('red') }}
+                    >
+                        {color === 'red' &&
+                            <FontAwesome5
+                                name={'check'}
+                                size={25}
+                                color={'#000000'}
+                            />
+                        }
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.color_blue}
+                        onPress={() => { setColor('blue') }}
+                    >
+                        {color === 'blue' &&
+                            <FontAwesome5
+                                name={'check'}
+                                size={25}
+                                color={'#000000'}
+                            />
+                        }
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.color_green}
+                        onPress={() => { setColor('green') }}
+                    >
+                        {color === 'green' &&
+                            <FontAwesome5
+                                name={'check'}
+                                size={25}
+                                color={'#000000'}
+                            />
+                        }
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.extra_row}>
+                    <TouchableOpacity
+                        style={styles.extra_button}
+                        onPress={() => { setShowBellModal(true) }}
+                    >
+                        <FontAwesome5
+                            name={'bell'}
+                            size={25}
+                            color={'#ffffff'}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={styles.extra_button}
+                        onPress={() => { navigation.navigate('Camera', { id: taskID }) }}
+                    >
+                        <FontAwesome5
+                            name={'camera'}
+                            size={25}
+                            color={'#ffffff'}
+                        />
+                    </TouchableOpacity>
+                </View>
+                {image ?
+                    <View>
+                        <Image
+                            style={styles.image}
+                            source={{ uri: image }}
+                        />
+                        <TouchableOpacity
+                            style={styles.delete}
+                            onPress={() => { deleteImage() }}
+                        >
+                            <FontAwesome5
+                                name={'trash'}
+                                size={25}
+                                color={'#ff3636'}
+                            />
+                        </TouchableOpacity>
+                    </View>
+                    :
+                    null
+                }
+                <View style={styles.checkbox}>
+                    <CheckBox
+                        value={done}
+                        onValueChange={(newValue) => setDone(newValue)}
+                    />
+                    <Text style={styles.text}>
+                        Is Done
+                    </Text>
+                </View>
+                <CustomButton
+                    title='Save Task'
+                    color='#1eb900'
+                    style={{ width: '100%' }}
+                    onPressFunction={setTask}
+                />
+            </View>
+        </ScrollView>
     )
 }
 
@@ -87,6 +277,10 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         padding: 10,
+    },
+    checkbox: {
+        flexDirection: 'row',
+        margin: 10,
     },
     input: {
         width: '100%',
@@ -98,5 +292,124 @@ const styles = StyleSheet.create({
         fontSize: 20,
         margin: 10,
         paddingHorizontal: 10,
+    },
+    text: {
+        fontSize: 20,
+        color: '#000000',
+    },
+    color_bar: {
+        flexDirection: 'row',
+        height: 50,
+        borderWidth: 2,
+        borderRadius: 10,
+        borderColor: '#555555',
+        marginVertical: 10,
+    },
+    color_white: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderTopLeftRadius: 10,
+        borderBottomLeftRadius: 10,
+    },
+    color_red: {
+        flex: 1,
+        backgroundColor: '#f28b82',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    color_blue: {
+        flex: 1,
+        backgroundColor: '#aecbfa',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    color_green: {
+        flex: 1,
+        backgroundColor: '#ccff90',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+    },
+    extra_row: {
+        flexDirection: 'row',
+        marginVertical: 10,
+    },
+    extra_button: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#0080ff',
+        borderRadius: 10,
+        marginHorizontal: 5,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    centered_view: {
+        flex: 1,
+        backgroundColor: '#00000099',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bell_modal: {
+        width: 300,
+        height: 200,
+        backgroundColor: '#ffffff',
+        borderRadius: 20,
+        borderWidth: 1,
+        borderColor: '#000000',
+    },
+    bell_body: {
+        height: 150,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bell_buttons: {
+        flexDirection: 'row',
+        height: 50,
+    },
+    bell_input: {
+        width: 50,
+        borderWidth: 1,
+        borderColor: '#555555',
+        borderRadius: 10,
+        backgroundColor: '#ffffff',
+        textAlign: 'center',
+        fontSize: 20,
+        margin: 10,
+    },
+    bell_cancel_button: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: '#000000',
+        borderBottomLeftRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bell_ok_button: {
+        flex: 1,
+        borderWidth: 1,
+        borderColor: '#000000',
+        borderBottomRightRadius: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    image: {
+        width: 300,
+        height: 300,
+        margin: 20,
+    },
+    delete: {
+        width: 50,
+        height: 50,
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        right: 20,
+        bottom: 20,
+        backgroundColor: '#ffffff80',
+        margin: 10,
+        borderRadius: 5,
     }
 })
